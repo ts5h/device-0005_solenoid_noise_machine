1cbcfd6faeb96e1c9ee50811377a5b089896cab7
at4 <at4@jc4.so-net.ne.jp>
2023-01-20 00:37:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Device-0005 / Solenoid Noise Machine
+An ATtiny85 based noise maker
---
+++ device-0005_solenoid_noise_machine/device-0005_solenoid_noise_machine.ino
@@ -0,0 +1,45 @@
+/*
+  Random Solenoid Noise Maker
+  ATtimy85
+  
+  ts5h / 0bjekt
+ */
+
+const int unsigned LED = 3;
+const int unsigned SOLENOID = 4;
+
+const int unsigned DUR = 15;
+
+int unsigned num;
+int unsigned randomDelay;
+
+void setup() {
+  randomSeed(analogRead(1));
+  
+  pinMode(LED, OUTPUT);
+  pinMode(SOLENOID, OUTPUT);
+
+  digitalWrite(LED, HIGH);
+  digitalWrite(SOLENOID, LOW);
+  delay(2000);
+}
+
+void loop() {
+  num = random(10);
+  if (num == 0) {
+    // ~5s interval
+    randomDelay = random(7971);
+  } else if (num <= 2) {
+    // ~1s interval
+    randomDelay = random(1971);
+  } else {
+    // ~250ms interval
+    randomDelay = random(221);
+  }
+  randomDelay += DUR;
+  
+  digitalWrite(SOLENOID, HIGH);
+  delay(DUR);
+  digitalWrite(SOLENOID, LOW);
+  delay(randomDelay);
+}
